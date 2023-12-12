42f47aa0e5044355611c4a537bb7a0429920ff4a
NGHIA <trongnghia.ngo@hotmail.com>
2023-12-12 14:03:09 +0700

    add test for memory in domain customer

---
+++ domain/customer/memory/memory_test.go
@@ -0,0 +1,50 @@
+package memory
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"github.com/ntnghiatn/ddd-go/aggregate"
+	"github.com/ntnghiatn/ddd-go/domain/customer"
+	"testing"
+)
+
+func TestMemoryRepo_Get(t *testing.T) {
+	type testCase struct {
+		name        string
+		id          uuid.UUID
+		expectedErr error
+	}
+	cust, err := aggregate.NewCustomer("Percy")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id := cust.GetID()
+	repo := MemoryRepo{
+		customers: map[uuid.UUID]aggregate.Customer{
+			id: cust,
+		},
+	}
+
+	testCases := []testCase{
+		{
+			name:        "no customer by id",
+			id:          uuid.MustParse("jkskfsk"),
+			expectedErr: customer.ErrCustomerNotFound,
+		},
+		{
+			name:        "customer by id",
+			id:          id,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := repo.Get(tc.id)
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected error %v get %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
